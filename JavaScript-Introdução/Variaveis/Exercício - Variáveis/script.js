86e5bd46750b73a79b86e7eb93efded0b0d6e4d1
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-01-20 10:47:31 -0300

    Create script.js

---
+++ JavaScript-Introdução/Variaveis/Exercício - Variáveis/script.js
@@ -0,0 +1,20 @@
+function desafio() {
+  let a = 5;
+  let b = 8;
+
+  /** Você não pode alterar o código acima */
+
+  /** Escreva seu código nas linhas 7 e 9 *
+  
+  
+  /** Você também não pode alterar esse código */
+  let c = 8;
+  b = a;
+  a = c;
+
+  console.log("A é? " + a);
+
+  console.log("B é? " + b);
+}
+
+desafio();
