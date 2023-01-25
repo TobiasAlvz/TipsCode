71f2023fe18cd1c558a277144bc4d6e7c3ab03f5
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-01-25 11:57:47 -0300

    Create script.js

---
+++ JavaScript-Introdução/Parâmetros e Argumentos2/script.js
@@ -0,0 +1,11 @@
+function getBread(custo) {
+  let saldo = 50;
+
+  console.log(
+    `O saldo atual é ${saldo}, você consegue comprar ${Math.floor(
+      saldo / custo
+    )} pães`
+  );
+}
+
+getBread(1.5);
