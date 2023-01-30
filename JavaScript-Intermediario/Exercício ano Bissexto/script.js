43e166c2f2aba8b6cf5d2f5d4cb0221ae88be1d2
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-01-30 11:39:41 -0300

    Update script.js

--- JavaScript-Intermediario/Exercício ano Bissexto/script.js
+++ JavaScript-Intermediario/Exercício ano Bissexto/script.js
@@ -2,6 +2,9 @@ function leapYear(year) {
   if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
     console.log(`O ano é ${year}, ele é bissexto`);
   } else {
-    console.log(`O ano é`);
+    console.log(`O ano é ${year}, ele não é bissexto`);
+    return year;
   }
 }
+
+leapYear(2000);
