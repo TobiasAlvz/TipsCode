8c8ac9eed844cd59ca73af51fb562c6402e4c10d
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-01-31 15:52:08 -0300

    Update script.js

--- JavaScript-Intermediario/array/script.js
+++ JavaScript-Intermediario/array/script.js
@@ -1,8 +1,8 @@
 const names = ["joao", "maria", "jose", "davi"];
 const newName = prompt();
 
-if (names.includes[newName]) {
-  alert("Nome incluido, acesso liberado");
+if (names.includes(newName)) {
+  alert("Nome incluido na lista, acesso liberado");
 } else {
   alert("Desculpe, seu nome não foi achado na lista");
 }
