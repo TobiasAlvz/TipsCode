2563ebe18c5c0a2c23a5a4b25eb936b9da066431
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-02-01 10:20:16 -0300

    Update script.js

--- JavaScript-Intermediario/Desafio FizzBuz/script.js
+++ JavaScript-Intermediario/Desafio FizzBuz/script.js
@@ -2,15 +2,18 @@ const array = [];
 let count = 1;
 
 function fizzBuz() {
-  array.push(count);
-  count++;
+  if (count % 3 === 0 && count % 5 === 0) {
+    array.push("FizzBuz");
+  }
+
   if (count % 3 === 0) {
     array.push("Fizz");
   } else if (count % 5 === 0) {
     array.push("Buz");
-  } else if (count % 3 === 0 && count % 5 === 0) {
-    array.push("FizzBuz");
+  } else {
+    array.push(count);
   }
+  count++;
   console.log(array);
 }
 
