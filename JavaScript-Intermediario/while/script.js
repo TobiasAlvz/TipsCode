3ac05d27128cae7702b7ef3bf8ec4859ab4f3208
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-02-02 11:26:56 -0300

    JavaScript.

--- JavaScript-Intermediario/while/script.js
+++ JavaScript-Intermediario/while/script.js
@@ -2,26 +2,20 @@ const array = [];
 let count = 1;
 
 function fizzBuz() {
-  if (count % 3 === 0 && count % 5 === 0) {
-    array.push("FizzBuz");
+  while (count <= 100) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      array.push("FizzBuz");
+    }
+    if (count % 3 === 0) {
+      array.push("Fizz");
+    } else if (count % 5 === 0) {
+      array.push("Buz");
+    } else {
+      array.push(count);
+    }
+    count++;
   }
-  if (count % 3 === 0) {
-    array.push("Fizz");
-  } else if (count % 5 === 0) {
-    array.push("Buz");
-  } else {
-    array.push(count);
-  }
-  count++;
   console.log(array);
 }
 
-fizzBuz();
-fizzBuz();
-fizzBuz();
-fizzBuz();
-fizzBuz();
-fizzBuz();
-
-fizzBuz();
 fizzBuz();
