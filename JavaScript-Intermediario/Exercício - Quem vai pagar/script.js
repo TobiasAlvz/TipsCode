08f87ccfe44d09dc647d32546a68a55c6e039ab3
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-02-02 11:15:35 -0300

    Update script.js

--- JavaScript-Intermediario/Exercício - Quem vai pagar/script.js
+++ JavaScript-Intermediario/Exercício - Quem vai pagar/script.js
@@ -2,6 +2,7 @@ const list = ["Isaac", "Pedro", "Maria", "João"];
 
 function sortear() {
   const randomNumber = Math.floor(Math.random() * list.length);
+
   console.log(`Quem vai pagar é ${list[randomNumber]}`);
 }
 
