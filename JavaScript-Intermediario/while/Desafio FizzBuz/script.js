99ffd088719e275b8d84def3f23b5754322735f5
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-02-02 11:22:04 -0300

    Create script.js

---
+++ JavaScript-Intermediario/while/Desafio FizzBuz/script.js
@@ -0,0 +1,27 @@
+const array = [];
+let count = 1;
+
+function fizzBuz() {
+  if (count % 3 === 0 && count % 5 === 0) {
+    array.push("FizzBuz");
+  }
+  if (count % 3 === 0) {
+    array.push("Fizz");
+  } else if (count % 5 === 0) {
+    array.push("Buz");
+  } else {
+    array.push(count);
+  }
+  count++;
+  console.log(array);
+}
+
+fizzBuz();
+fizzBuz();
+fizzBuz();
+fizzBuz();
+fizzBuz();
+fizzBuz();
+
+fizzBuz();
+fizzBuz();
