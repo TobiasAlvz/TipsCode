386edcd5bfb45739f4f71b4790f0b7f7d40b02a8
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-02-20 20:14:00 -0300

    Update script.js

--- JavaScript-Intermediario/for/for2/script.js
+++ JavaScript-Intermediario/for/for2/script.js
@@ -1,1 +1,26 @@
-const list = ["lisa", "arthur", "luke", "mat"];
+let alunos = ["Tobias", "Gabriel", "Ana"];
+let notaA = [10, 7, 8];
+let notaB = [9, 6, 7];
+
+let media = function (nota1, nota2) {
+  return (nota1 + nota2) / 2;
+};
+
+let situacao = function (media) {
+  if (media >= 7) {
+    return "Aprovado";
+  } else {
+    return "Reprovado";
+  }
+};
+for (let index in alunos) {
+  let nota1 = notaA[index];
+  let nota2 = notaB[index];
+  let m = media(nota1, nota2);
+  const element = alunos[index];
+  console.log(
+    `Aluno ${element} -> nota1 - ${nota1} -> nota2 - ${
+      notaB[index]
+    } -> media - ${media(nota1, nota2)} -> situação - ${situacao(m)}`
+  );
+}
