b966b897dfaf2cf255ed7320e533837bddd72dfb
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-03-14 15:10:50 -0300

    Update script.js

--- JavaScript-DOM/Introdução/script.js
+++ JavaScript-DOM/Introdução/script.js
@@ -1,4 +1,7 @@
 const title = document.getElementById("title");
+const list = document.querySelector("li");
 
 title.innerHTML += " Tobias";
 title.style.color = "red";
+
+list.innerHTML = "Conteudo mudado";
