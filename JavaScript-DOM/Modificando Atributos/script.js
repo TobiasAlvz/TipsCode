89832caf30f22d50f1078b706defa708a1d72731
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-03-27 10:30:02 -0300

    Create script.js

---
+++ JavaScript-DOM/Modificando Atributos/script.js
@@ -0,0 +1,5 @@
+function show() {
+  // O toggle retorna um valor booleano, se for true. ele tira a classe.Se for False, ele adiciona a classe
+
+  title = document.querySelector("#title").classList.toggle("t1");
+}
